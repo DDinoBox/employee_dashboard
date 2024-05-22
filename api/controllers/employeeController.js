033ba1de6f1c772f3923eb1c84db701e744c2567
DDinoBox <dinobox.create@gmail.com>
2024-05-22 20:11:57 +0900

    Feat: 특정 기간 직원수(남녀별), 급여(부서별), 부서정보(각부서별부서장), 12개월간 입.퇴사자수 가져오기 구현

--- api/controllers/employeeController.js
+++ api/controllers/employeeController.js
@@ -1,6 +1,52 @@
 import employeeService from '../services/employeeService.js';
 import { catchAsync } from '../utils/error.js';
 
+const getDepartmentSalaryAndEmployeeStats = async (req, res, next) => {
+  try {
+    const { search_date = '1993-01-31' } = req.query;
+
+    if (!search_date) {
+      const error = new Error('Search date is required');
+      error.status = 404;
+      throw error;
+    }
+
+    const salaryByDept = await employeeService.getSalaryByDept(search_date);
+
+    const totalSalary = salaryByDept.reduce((total, dept) => total + BigInt(dept.total_salary), BigInt(0)).toString();
+
+    const [{ total_employees, male_count, female_count }] = await employeeService.getEmpCountByGender(search_date);
+
+    const jobTurnover = await employeeService.jobTurnover(search_date);
+
+    let getDepartmentInfo = await employeeService.getDepartmentInfo(search_date);
+
+    getDepartmentInfo = await Promise.all(getDepartmentInfo.map(async (deptInfo) => {
+      const matchingSalaryDept = salaryByDept.find(salaryDept => salaryDept.dept_no === deptInfo.dept_no);
+      if (matchingSalaryDept) {
+        return {
+          ...deptInfo,
+          total_employees: matchingSalaryDept.total_employees
+        };
+      } else {
+        return deptInfo;
+      }
+    }));
+
+    res.status(200).json({
+      salaryByDept,
+      totalSalary,
+      total_employees,
+      male_count,
+      female_count,
+      jobTurnover,
+      getDepartmentInfo
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 const getEmployeeList = catchAsync(async (req, res) => {
   const { page = 1, limit = 15, dep_no = 'd005', search_date = '1993-01-31' } = req.query;
   const [employeeList, totalPages] = await employeeService.getEmployeeList(page, limit, dep_no, search_date);
@@ -14,6 +60,7 @@ const getEmployeeTurnoverDetails = catchAsync(async (req, res) => {
 });
 
 export default {
+  getDepartmentSalaryAndEmployeeStats,
   getEmployeeList,
   getEmployeeTurnoverDetails
 };
